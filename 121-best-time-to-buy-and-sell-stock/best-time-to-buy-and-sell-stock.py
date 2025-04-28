6963a2366e24d34ea3503f205b966811150a5234
Ahmed mousa <mahmed20156947@gmail.com>
2025-04-28 10:59:10 +0300

    Time: 124 ms (33.00%) | Memory: 27 MB (8.98%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -9,3 +9,16 @@ class Solution:
             max_profit = max(max_profit, price - min_price)  
 
         return max_profit
+
+        # n = len(prices)
+        # if n == 0:
+        #     return 0
+
+        # dp = [0] * n  # dp[i] = max profit if we sell on day i
+        # min_price = prices[0]
+
+        # for i in range(1, n):
+        #     min_price = min(min_price, prices[i])
+        #     dp[i] = max(dp[i - 1], prices[i] - min_price)
+
+        # return dp[-1]
