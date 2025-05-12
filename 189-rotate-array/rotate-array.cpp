23a72ec0648a502ebe811b80cbfd01dae7ba75be
Ahmed mousa <mahmed20156947@gmail.com>
2025-05-12 15:58:46 +0300

    Time: 0 ms (100.00%) | Memory: 29.4 MB (97.02%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -7,8 +7,5 @@ public:
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin()+k);
          reverse(nums.begin()+k,nums.end());
-        for(int i=0;i<n;i++){
-            cout<<nums[i];
-        }
     }
 };
