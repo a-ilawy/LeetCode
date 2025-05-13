00f016a20214edf29211cb6cdc3cfbfa1e5dff69
Ahmed mousa <mahmed20156947@gmail.com>
2025-05-13 18:41:24 +0300

    Time: 22 ms (28.37%) | Memory: 20.5 MB (62.33%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-    //      if (nums.size() == 1) {
-    //     return 0;
-    // }
-    // if (nums.size() == 2 && nums[0] >= 1) {
-    //     return 1;
-    // }
 
-    int jumpSteps = 0;
-    int goalIndex = nums.size() - 1;
+        int jumpSteps = 0;
+        int goalIndex = nums.size() - 1;
 
-    while (goalIndex > 0) {
-        for (int i = 0; i < goalIndex; ++i) {
-            if (i + nums[i] >= goalIndex) {
-                goalIndex = i;
-                jumpSteps++;
-                break;
+        while (goalIndex > 0) {
+            for (int i = 0; i < goalIndex; ++i) {
+                if (i + nums[i] >= goalIndex) {
+                    goalIndex = i;
+                    jumpSteps++;
+                    break;
+                }
             }
         }
-    }
 
-    return jumpSteps;
+        return jumpSteps;
     }
 };
