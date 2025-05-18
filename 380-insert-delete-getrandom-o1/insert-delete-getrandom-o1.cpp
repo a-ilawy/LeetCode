7e9baa6b269fe89d8f542ee25eacb20b89db717f
Ahmed mousa <mahmed20156947@gmail.com>
2025-05-18 16:43:35 +0300

    Time: 36 ms (86.24%) | Memory: 113.4 MB (18.66%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,46 +1,54 @@
 class RandomizedSet {
 public:
-    set<int> s = {};
+    /**
+     * we need to make any operation in O(1)
+     * vector good in insert and return (get)
+     * map good in search and delete
+     */
+    unordered_map<int, int> s;
+    vector<int> a;
     RandomizedSet() {}
 
     bool insert(int val) {
         auto it = s.find(val);
 
         if (it == s.end()) {
-            s.insert(val);
+            s[val] = a.size();
+            a.push_back(val);
             return true;
         } else
             return false;
     }
 
     bool remove(int val) {
-
-        auto it = s.find(val);
-
-        if (it != s.end()) {
-            s.erase(val);
-            return true;
-        } else
+        
+        if (s.find(val) == s.end()) {
             return false;
+        }
+
+        int idx = s[val];    
+        int last = a.back(); 
+     
+        a[idx] = last; 
+        s[last] = idx; 
+
+        a.pop_back(); 
+        s.erase(val); 
+
+        return true;
     }
 
     int getRandom() {
-        size_t index = rand() % s.size();
+        size_t index = rand() % a.size();
 
-     
-        auto it = s.begin();
-
-       
-        advance(it, index);
-
-        return *it; 
+        return a[index];
     }
-    };
+};
 
-    /**
-     * Your RandomizedSet object will be instantiated and called as such:
-     * RandomizedSet* obj = new RandomizedSet();
-     * bool param_1 = obj->insert(val);
-     * bool param_2 = obj->remove(val);
-     * int param_3 = obj->getRandom();
-     */
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
