70dd5e86338a3b97bbccdbfcba52264c84304b8a
Ahmed mousa <mahmed20156947@gmail.com>
2025-05-19 14:54:41 +0300

    Time: 228 ms (5.06%) | Memory: 22.9 MB (43.58%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> res(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                res[i] = res[i-1]+1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                if (res[i] <= res[i + 1]) {
+                    res[i] = res[i+1]+1;
+                }
+            }
+        }
+        int totalCandy=0;
+        for(int i=0; i<n;i++){
+            cout<<res[i]<<",";
+            totalCandy+=res[i];
+        }
+        return totalCandy;
+    }
+};
