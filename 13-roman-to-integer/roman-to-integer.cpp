03e9509f4de5a67e832c4fd1d202790750fc0f08
Ahmed mousa <mahmed20156947@gmail.com>
2025-05-26 15:53:42 +0300

    Time: 8 ms (40.03%) | Memory: 10 MB (84.27%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int romanSymbolToValue(char c) {
+        switch (c) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        default:
+            return 1000;
+        }
+    }
+
+    int romanToInt(string s) {
+        int res = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = romanSymbolToValue(s[i]);
+            if (i > 0) {
+                int nextValue = romanSymbolToValue(s[i - 1]);
+                if (currValue > nextValue) {
+                    cout<<currValue<<" , "<<nextValue<<endl;
+                    res += currValue - nextValue;
+                    i--;
+                } else {
+                    res += currValue;
+                }
+            }
+
+            else {
+                res += currValue;
+            }
+        }
+
+        return res;
+    }
+};
