f89a60387b6be11f2190893a616209b10689c69d
Ahmed mousa <mahmed20156947@gmail.com>
2025-05-31 01:20:27 +0300

    Time: 0 ms (100.00%) | Memory: 12.1 MB (33.96%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,15 +4,18 @@ public:
         int minLen = strs[0].length();
         int idx = 0;
         for(int i=0;i<strs.size();i++){
-            if(strs[i].length() <= minLen){
-                minLen = strs[i].length();
+            int currLen = strs[i].length();
+            // cout<<currLen<<" , "<<minLen<<endl;
+            if(currLen <= minLen){
+                minLen = currLen;
                 idx = i;
             }
         }
         string prefix = strs[idx];
-        for(auto currStr : strs){
+        for(int i=0;i<strs.size();i++){
            
-           if(currStr!=prefix){
+           if(i!=idx){
+            string currStr = strs[i];
             int pIdx = 0;
             for(int j=0; j<prefix.length(); j++){
                 // cout<<currStr<<" , "<<prefix<<endl;
