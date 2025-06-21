b27f866c84bc3575898f6a7de59b5380e97349c6
Ahmed mousa <mahmed20156947@gmail.com>
2025-06-21 10:43:02 +0300

    Time: 0 ms (100.00%) | Memory: 10 MB (58.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-    //     int n = s.length();
-    //     if(n<1) return true;
+        int n = s.length();
+        if(n<1) return true;
 
-    //     int l=0;
-    //     int r = n-1;
-    //    while(l<r) {
-    //         while ( l<n && !isalnum(static_cast<unsigned char>(s[l]))){
-    //             l++;
-    //         }
-    //         if(l==n) return true;
-    //         while (r > 0 && !isalnum(static_cast<unsigned char>(s[r]))) r--;
-          
-    //         if(tolower(s[l]) != tolower(s[r])) return false;
-    //         else{
-    //             l++;
-    //             r--;
-    //         }
-           
-    //     }
-    //     return true;
-        int left = 0, right = s.length() - 1;
-
-            while (left < right) {
-                // Skip non-alphanumeric characters from the left
-                while (left < right && !isalnum(s[left])) left++;
-
-                // Skip non-alphanumeric characters from the right
-                while (left < right && !isalnum(s[right])) right--;
-
-                // Compare characters case-insensitively
-                if (tolower(s[left]) !=tolower(s[right])) {
-                    return false;
-                }
-
-                left++;
-                right--;
+        int l=0;
+        int r = n-1;
+       while(l<r) {
+            while ( l<n && !isalnum(static_cast<unsigned char>(s[l]))){
+                l++;
             }
-
-            return true;
+            if(l==n) return true;
+            while (r > 0 && !isalnum(static_cast<unsigned char>(s[r]))) r--;
+          
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            else{
+                l++;
+                r--;
+            }
+           
+        }
+        return true;
     }
 };
