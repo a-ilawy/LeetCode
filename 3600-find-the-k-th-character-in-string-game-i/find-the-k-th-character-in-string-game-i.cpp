a2301f1cf04e6c3f2c871c5f0a1eeb9d7e9ac580
Ahmed mousa <mahmed20156947@gmail.com>
2025-07-04 21:42:21 +0300

    Time: 21 ms (9.32%) | Memory: 10.1 MB (41.69%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        int size = 0;
+        string s="a";
+        while(size < k){
+            string newPart = "";
+            for(auto c:s){
+                if (c=='z') newPart+='a';
+                else{
+                    newPart+= char(int(c)+1);
+                }
+            }
+          
+            s+=newPart;
+            cout<<newPart<<" --> "<<s<<endl;
+            size = s.length();
+        }
+        return s[k-1];
+    }
+};
+// bccd -> abbcbccd
+//bccdcdde -> abbcbccdbccdcdde
