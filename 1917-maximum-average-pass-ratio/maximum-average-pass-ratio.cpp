fc1fc79d5d479a6fa31c6820b3e493f10cf184a0
Ahmed mousa <mahmed20156947@gmail.com>
2025-09-08 15:16:07 +0300

    Time: 276 ms (75.67%) | Memory: 98 MB (43.04%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -3,7 +3,6 @@ public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         priority_queue<pair<double, int>> pq;
         int n = classes.size();
-        double res=0;
         
         for(int i=0;i<n;i++){
             double diff = (double)(classes[i][0]+1)/(double)(classes[i][1]+1) -
@@ -24,11 +23,11 @@ public:
             pq.push({diff, indx});
         }
        
-        while(!pq.empty()){
-            int indx = pq.top().second;
-            res+=(double)(classes[indx][0])/(double)(classes[indx][1]);
-            pq.pop();
+        double sum = 0;
+        for (auto& c : classes) {
+            sum += (double)c[0] / c[1];
         }
-        return (res/n);
+        
+        return sum / n;
     }
 };
