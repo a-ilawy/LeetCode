5269148840d44948c7e8d6bc1a83004087339410
Ahmed mousa <mahmed20156947@gmail.com>
2025-09-19 21:27:24 +0300

    Time: 4 ms (46.54%) | Memory: 11.8 MB (75.07%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -3,9 +3,16 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         if(ransomNote.length() > magazine.length()) return false;
         unordered_map<char, int> mp;
-        for(int i=0;i<ransomNote.length();i++) mp[ransomNote[i]]+=1;
-        for(int i=0;i<magazine.length();i++) if(mp[magazine[i]]>0)mp[magazine[i]]--;
-        for(int i=0;i<ransomNote.length();i++) if (mp[ransomNote[i]]!=0) return false;
+        // for(int i=0;i<ransomNote.length();i++) mp[ransomNote[i]]+=1;
+        // for(int i=0;i<magazine.length();i++) if(mp[magazine[i]]>0)mp[magazine[i]]--;
+        // for(int i=0;i<ransomNote.length();i++) if (mp[ransomNote[i]]!=0) return false;
+        // return true;
+
+        for(int i=0;i<magazine.length();i++) mp[magazine[i]]++;
+        for(int i=0;i<ransomNote.length();i++){
+             if(mp[ransomNote[i]]==0)return false; 
+             mp[ransomNote[i]]--;
+        }
         return true;
 
     }
