da4f41ab9fb6fa09f9f043e7a717fca5aaac8160
Ahmed mousa <mahmed20156947@gmail.com>
2025-09-19 21:27:59 +0300

    Time: 6 ms (32.46%) | Memory: 11.9 MB (28.49%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        // if(ransomNote.length() > magazine.length()) return false;
+        if(ransomNote.length() > magazine.length()) return false;
         unordered_map<char, int> mp;
         // for(int i=0;i<ransomNote.length();i++) mp[ransomNote[i]]+=1;
         // for(int i=0;i<magazine.length();i++) if(mp[magazine[i]]>0)mp[magazine[i]]--;
