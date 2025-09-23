f7a6b410734f455167ff8c9a582b88de8df0b430
Ahmed mousa <mahmed20156947@gmail.com>
2025-09-23 11:11:32 +0300

    Time: 3 ms (24.28%) | Memory: 23.2 MB (67.08%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
+    // void printFreq(vector<int>& freq){
+    //     cout<<endl;
+    //     for(int i=0; i< freq.size(); i++){
+    //         cout<<freq[i]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
     int maxFrequencyElements(vector<int>& nums) {
         vector<int> freq (101, 0);
         for(int i=0; i< nums.size(); i++){
             freq[nums[i]]++;
            
         }
-        
-        sort(freq.begin(), freq.end(), greater<int>());
-       
-        int cnt = freq[0];
+
         int largeFreq = freq[0];
-        for(int i=1; i< freq.size(); i++){
-            if(freq[i] == largeFreq) {cnt +=freq[1];}
-            else{
-                break;
+        int indx = 0;
+        for(int i=0; i< freq.size(); i++){
+            if(freq[i] > largeFreq){
+                largeFreq = freq[i];
+                indx=i;
+            }
+        }
+        
+        int cnt = largeFreq;
+        for(int i=0; i< freq.size(); i++){
+            if( i != indx && freq[i] == largeFreq) {
+                cnt +=freq[i];
             }
         }
         return cnt;
-
     }
 };
